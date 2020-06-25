2ce562d6f8958af13ebcb59aadf1e0f44c2f4b34
Ryan Johnson <ryanleejwebdev@gmail.com>
2020-06-24 20:01:11 -0500

    Manage the state with setState hooks.

--- src/components/SearchMovies.js
+++ src/components/SearchMovies.js
@@ -1,11 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function SearchMovies() {
+  // states - inputQuery & movies
+  //// useState(defaultValue) - returns an array of 2 things
+  //////- 1.) the actual state, thing you want to track
+  //////- 2.) a function that will update that state
+  //////vv Array Destructuringvv
+  const [query, setQuery] = useState("");
+  //  create the state for the movies, and update that state when appropriate.
+  const [movies, setMovies] = useState([]);
+
   const searchMovies = async (e) => {
     e.preventDefault();
-    console.log("submit!");
-
-    const query = "Jurassic Park";
 
     const url = `https://api.themoviedb.org/3/search/movie?api_key=f81d49004876b0a9804f72e61282e832&language=en-US&query=${query}&page=1&include_adult=false`;
 
@@ -15,7 +21,8 @@ export default function SearchMovies() {
       // res.json will return a promise
       const data = await res.json();
 
-      console.log(data);
+      console.log(data.results);
+      setMovies(data.results);
     } catch (err) {
       console.error(err);
     }
@@ -31,10 +38,15 @@ export default function SearchMovies() {
         className="input"
         name="query"
         placeholder="i.e. Jurassic Park"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
       />
       <button className="button" type="submit">
         Search
       </button>
     </form>
+    // <div className="movies">
+
+    // </div>
   );
 }
