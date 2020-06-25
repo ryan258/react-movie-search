20d04a57b9ed7cbb7cd4db41e3abe5d285d71941
Ryan Johnson <ryanleejwebdev@gmail.com>
2020-06-24 19:34:25 -0500

    Create a search function that connects to the API.

--- src/components/SearchMovies.js
+++ src/components/SearchMovies.js
@@ -1,8 +1,28 @@
 import React from "react";
 
 export default function SearchMovies() {
+  const searchMovies = async (e) => {
+    e.preventDefault();
+    console.log("submit!");
+
+    const query = "Jurassic Park";
+
+    const url = `https://api.themoviedb.org/3/search/movie?api_key=f81d49004876b0a9804f72e61282e832&language=en-US&query=${query}&page=1&include_adult=false`;
+
+    try {
+      // res well hold the raw response
+      const res = await fetch(url);
+      // res.json will return a promise
+      const data = await res.json();
+
+      console.log(data);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   return (
-    <form className="form">
+    <form className="form" onSubmit={searchMovies}>
       <label htmlFor="query" className="label">
         Movie Name
       </label>
